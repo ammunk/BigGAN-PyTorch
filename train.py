e2fd12fc6728b7e5e5f6ab1700765d499403a0aa
Andreas Munk <andreas@ammunk.com>
2020-09-10 13:09:52 -0700

    redo comment

--- train.py
+++ train.py
@@ -251,7 +251,7 @@ def run(config):
                               z_=z_, y_=y_, config=config)
 
   print('Beginning training at epoch %d...' % state_dict['epoch'])
-  Train for specified number of epochs, although we mostly track G iterations.
+  # log metrics
   Gsub = GeneratorSubstitute(G, z_, y_, fixed_z, fixed_y)
   Gemasub = GeneratorSubstitute(G_ema, z_, y_, fixed_z, fixed_y)
   wandbwrapper.fid_score(Gsub, loaders[0].dataset,
@@ -266,6 +266,7 @@ def run(config):
                             N=int(1e3), label='small-ema')
   wandbwrapper.log(0, 0.)
 
+  # Train for specified number of epochs, although we mostly track G iterations.
   for epoch in range(state_dict['epoch'], config['num_epochs']):
 
     # Which progressbar to use? TQDM or my own?
