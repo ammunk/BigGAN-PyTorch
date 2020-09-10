8577a1c62ea0fde2b67c4a778f2d0798ddaafa94
Andreas Munk <andreas@ammunk.com>
2020-09-09 22:38:36 -0700

    update train

--- train_fns.py
+++ train_fns.py
@@ -21,7 +21,7 @@ def dummy_training_function():
 
 def GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config):
   reg_strength = config['reg_strength']
-  advas = AdversarysAssistant(reg_strength)
+  advas = AdversarysAssistant(1 if reg_strength < 0 else reg_strength)
   additional_metric = {}
   def train(x, y, advas=advas, additional_metric=additional_metric):
     G.optim.zero_grad()
@@ -54,8 +54,8 @@ def GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config):
         D_fake, D_real = GD(z_[:config['batch_size']], y_[:config['batch_size']], 
                             x[counter], y[counter], train_G=False, 
                             split_D=config['split_D'])
-         
-        # Compute components of D's loss, average them, and divide by 
+
+        # Compute components of D's loss, average them, and divide by
         # the number of gradient accumulations
         D_loss_real, D_loss_fake = losses.discriminator_loss(D_fake, D_real)
         D_loss = (D_loss_real + D_loss_fake) / float(config['num_D_accumulations'])
@@ -72,7 +72,8 @@ def GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config):
     
     # Optionally toggle "requires_grad"
     if config['toggle_grads']:
-      utils.toggle_grad(D, False)
+      if reg_strength == 0:
+        utils.toggle_grad(D, False)
       utils.toggle_grad(G, True)
       
     # Zero G's gradients by default before training G, for safety
@@ -80,7 +81,8 @@ def GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config):
     reg_loss = None
     counter = 0
     # If accumulating gradients, loop multiple times
-    for accumulation_index in range(config['num_G_accumulations']):    
+    for accumulation_index in range(config['num_G_accumulations']):
+
       z_.sample_()
       y_.sample_()
       D_fake = GD(z_, y_, train_G=True, split_D=config['split_D'])
@@ -91,21 +93,25 @@ def GAN_training_function(G, D, GD, z_, y_, ema, state_dict, config):
       if reg_strength == 0:
         G_loss.backward()
       elif config['num_G_accumulations'] == 1:
+        print(x_reg)
         D_real = GD.D(x_reg[counter], y_reg[counter])
-        D_loss_real, D_loss_fake = losses.loss_no_hinge_dos(D_fake, D_real)
+        D_loss_real, D_loss_fake = losses.loss_no_hinge_dis(D_fake, D_real)
         D_loss = D_loss_real + D_loss_fake
         advas.regularize(GD.D.parameters(),
                          D_loss*D_real.size(0))
-        advas_loss = advas.aggregrate_grads(D_real.size(0))
+
+        advas_loss = advas.aggregate_grads(D_real.size(0))
         if reg_strength == -1:
-          advas.normalized_backward(GD.G.parameters(), G_loss, advas_loss,
-                                    retain_first_graph=True)
+          advas.normalized_backward(G.parameters(), G_loss,
+                                    advas_loss, retain_first_graph=True)
         elif reg_strength == -2:
-          advas.normalized_advas_backward(GD.G.parameters(), G_loss, advas_loss,
-                                          retain_first_graph=True)
+          advas.normalized_advas_backward(G.parameters(), G_loss,
+                                          advas_loss, retain_first_graph=True)
         else:
           (G_loss + advas_loss).backward()
         additional_metric = {'advas_loss': advas_loss.item()}
+        if config['toggle_grads']:
+          utils.toggle_grad(D, False)
       else:
         raise ValueError("Either turn off adversarys assistant or "
                          + "set accumulations to 1")
